ce201b1294a2b5e7ccf79e34ce4ea9a1f6f9421e
sofiapantas <sofiasarabia0831@gmail.com>
2025-08-02 02:45:14 +0800

    added cors and corrected route errors

--- server.js
+++ server.js
@@ -1,21 +1,32 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./config/db');
 
 const swaggerRoute = require("./routes/swagger");
+const authRoutes = require('./routes/authRoutes');
+const productRoutes = require('./routes/productRoutes');
+const userRoutes = require('./routes/userRoutes');
+const mainRoutes = require('./routes/index');
+
+const swaggerSpec = require('./config/swagger');
+const swaggerUi = require('swagger-ui-express');
 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
 // Swagger
-app.use("/", swaggerRoute); 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.use(swaggerSpec));
+
 
 // Routes
 app.use('/auth', authRoutes);
 app.use('/products', productRoutes);
+app.use('/users', userRoutes);
 app.use('/', mainRoutes); // optional if your app needs it
 
 // Centralized Error Handler
