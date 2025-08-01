b4c87a07a957ee876c7781bde3d6938efa92d82c
camilamoyac <cami.csmc@gmail.com>
2025-08-01 14:14:54 -0400

    implemented swagger autogen and routes

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
-const productRoutes = require('./routes/productRoutes');
-const mainRoutes = require('./routes'); // from your original
 
-const swaggerSpec = require('./config/swagger');
-const swaggerUi = require('swagger-ui-express');
+const swaggerRoute = require("./routes/swagger");
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,12 +11,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 // Swagger
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/", swaggerRoute); 
 
 // Routes
-app.use('/auth', authRoutes);
-app.use('/products', productRoutes);
-app.use('/', mainRoutes); // optional if your app needs it
+app.use('/', require('./routes'));
 
 // Centralized Error Handler
 app.use((err, req, res, next) => {
