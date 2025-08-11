9b27055bae8d5c6ef6be365c872445a1d13c3e30
sofiapantas <sofiasarabia0831@gmail.com>
2025-08-12 01:28:28 +0800

    added more fields to the user model

--- models/User.js
+++ models/User.js
@@ -1,6 +1,18 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  userName: {
+    type: String,
+    required: true
+  },
   email: { 
     type: String, 
     required: true, 
@@ -15,7 +27,14 @@ const userSchema = new mongoose.Schema({
     type: String, 
     enum: ['admin', 'customer'], 
     default: 'customer' 
+  },
+  address: {
+    type: String,
+  },
+  number: {
+    type: String
   }
+
 });
 
 module.exports = mongoose.model('User', userSchema);
